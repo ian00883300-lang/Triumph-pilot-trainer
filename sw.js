4f859bfb128c20d6ed50a374fd26a4535d8cecf6
ian00883300-lang <ian00883300@gmail.com>
2026-09-23 21:54:13 +0800

    Release v216: expand and review math formula cards

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-/* TRIUMPH Pilot Trainer v215 service worker
+/* TRIUMPH Pilot Trainer v216 service worker
    Online navigation: network first (bypass browser HTTP cache).
    Offline fallback: most recently fetched index.html.
 */
-const CACHE_NAME = 'triumph-pilot-v215';
+const CACHE_NAME = 'triumph-pilot-v216';
 const INDEX_FALLBACK = './index.html';
 
 self.addEventListener('install', () => {
